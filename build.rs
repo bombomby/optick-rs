ce896704730ca5f14ce405ddea5210c4736166ec
bombom_by <nb_grif@mail.ru>
2020-08-05 13:51:50 +0100

    Speculative fix for #1. Adding MT and MD variations of the lib.

--- build.rs
+++ build.rs
@@ -1,7 +1,18 @@
+fn runtime() -> &'static str {
+    if std::env::var("CARGO_CFG_TARGET_FEATURE")
+        .unwrap_or_default()
+        .contains("crt-static")
+    {
+        "MT"
+    } else {
+        "MD"
+    }
+}
+
 fn main() {
     println!(
         "cargo:rustc-link-search={}/external/lib/x64/",
         std::env!("CARGO_MANIFEST_DIR")
     );
-    println!("cargo:rustc-link-lib=OptickCore")
+    println!("cargo:rustc-link-lib=OptickCore_{}", runtime())
 }
